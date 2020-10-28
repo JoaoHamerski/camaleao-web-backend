a3d3e69804f462f9efe7f44def07063b6450d71e
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2020-10-28 13:56:09 -0300

    ads

--- resources/views/orders/pdf/order.blade.php
+++ resources/views/orders/pdf/order.blade.php
@@ -45,7 +45,7 @@
 <body>
 	@if (isset($order->getPaths('art_paths')[0]))
 		<div class="text-center">
-			<img src="{{ Helper::imageTo64(public_path($order->getPaths('art_paths')[0])) }} ">
+			<img class="text-center" src="{{ Helper::imageTo64(public_path($order->getPaths('art_paths')[0])) }} ">
 		</div>
 	@else
 		[sem imagem cadastrada]
