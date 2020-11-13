e2c0c4e94895952c1a14f495e8ec8c580be80aa6
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2020-11-13 18:11:59 -0300

    web-app

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,8 @@ require('./bootstrap');
 require('./cleave');
 require('./_custom');
 
+window.addEventListener('load', () => {
+	if ('servieWorker' in navigator) {
+		navigator.servieWorker.register('_service-worker.js');
+	}
+})
