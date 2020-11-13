8da22ff6d8de46ddb5d01983650b0499b460c2ae
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2020-11-13 18:45:55 -0300

    ajustes

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,13 +9,10 @@ window.addEventListener('load', () => {
 	}
 });
 
-let deferredPrompt;
-
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
+  
   // Update UI notify the user they can install the PWA
   showInstallPromotion();
 });
