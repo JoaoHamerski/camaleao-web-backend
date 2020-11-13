9f5680c04329c0ee54f0969fceb57aaf10ee150f
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2020-11-13 18:18:51 -0300

    assets compilados

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,4 +7,15 @@ window.addEventListener('load', () => {
 	if ('servieWorker' in navigator) {
 		navigator.servieWorker.register('_service-worker.js');
 	}
-})
+});
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+});
