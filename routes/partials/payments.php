557e4eb152e40621bb49349e813d02c2ca97d597
João Antônio <joaohamerski@hotmail.com>
2021-09-02 17:36:39 -0300

    Ajustes

--- routes/partials/payments.php
+++ routes/partials/payments.php
@@ -4,17 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentsController;
 
 Route::prefix('cliente')->middleware('role:gerencia,atendimento')->group(function () {
-    Route::post('/cliente/{client}/pedido/{order}/new-payment', [
+    Route::post('/{client}/pedido/{order}/new-payment', [
         PaymentsController::class,
         'store'
     ])->name('store');
 
-    Route::get('/cliente/{client}/pedido/{order}/pagamento/{payment}/get-change-payment-view', [
+    Route::get('/{client}/pedido/{order}/pagamento/{payment}/get-change-payment-view', [
         PaymentsController::class,
         'getChangePaymentView'
     ]);
 
-    Route::post('/cliente/{client}/pedido/{order}/pagamento/{payment}', [
+    Route::post('/{client}/pedido/{order}/pagamento/{payment}', [
         PaymentsController::class,
         'patch'
     ]);
