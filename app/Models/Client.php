b29b948465bfb4beb17806a39aa196762a2151d5
João Antônio <joaohamerski@hotmail.com>
2021-12-21 19:22:28 -0300

    🔥 Remove deprecated FileManger trait

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
-use App\Traits\FileManager;
 use App\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Client extends Model
 {
-    use HasFactory, FileManager, LogsActivity;
+    use HasFactory, LogsActivity;
 
     protected $guarded = [];
 
