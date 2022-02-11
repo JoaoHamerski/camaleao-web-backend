57a630b23abb58d25bc7a80e9a0f6261fea4aa42
João Antônio <joaohamerski@hotmail.com>
2022-02-11 20:26:59 -0300

    🎨 Formatting ExpenseType model

--- app/Models/ExpenseType.php
+++ app/Models/ExpenseType.php
@@ -10,59 +10,15 @@ class ExpenseType extends Model
 {
     use HasFactory, LogsActivity;
 
-    protected $guarded = [];
-    protected static $logName = 'expenses_type';
-    protected static $logUnguarded = true;
-    protected static $logOnlyDirty = true;
-    
-    /**
-     * Descrição que é cadastrada no log de atividades toda vez que um tipo
-     * de evento ocorre no model
-     * 
-     * @param string $eventname
-     * 
-     * @return string
-     */
-    public function getDescriptionForEvent(string $eventName): string
-    {
-	    if ($eventName == 'created') {
-	        return '
-	            <div data-event="created">
-	                <strong>:causer.name</strong>
-	                criou um novo tipo de despesa
-	                <strong>":subject.name"</strong>
-	            </div>
-	        ';
-    	}
-
-    	if ($eventName == 'updated') {
-    		return '
-    			<div data-event="updated">
-    				<strong>:causer.name</strong>
-    				alterou o tipo de despesa
-    				<strong>":subject.name"</strong>
-    			</div>
-    		';
-    	}
-
-    	if ($eventName == 'deleted') {
-    		return '
-    			<div data-event="deleted">
-    				<strong>:causer.name</strong>
-    				deletou o tipo de despesa
-    				<strong>"subject.name"</strong>
-    			</div>
-    		';
-    	}
-    }
+    protected $fillable = ['name'];
 
     /**
      * Um tipo de despesa tem varias despesas
-     * 
+     *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function expenses()
     {
-    	return $this->hasMany(Expense::class);
+        return $this->hasMany(Expense::class);
     }
 }
