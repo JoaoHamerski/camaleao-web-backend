46bd50cae78a48ad3976509b0a776079d61ed72f
João Antônio <joaohamerski@hotmail.com>
2022-02-11 20:18:10 -0300

    ✨ Add CanAccess directive

---
+++ app/GraphQL/Directives/CanAccessDirective.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\GraphQL\Directives;
+
+use Closure;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Schema\Values\FieldValue;
+use Nuwave\Lighthouse\Exceptions\DefinitionException;
+use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use Nuwave\Lighthouse\Support\Contracts\FieldMiddleware;
+
+class CanAccessDirective extends BaseDirective implements FieldMiddleware
+{
+    // TODO implement the directive https://lighthouse-php.com/master/custom-directives/getting-started.html
+
+    public static function definition(): string
+    {
+        return
+            /** @lang GraphQL */
+            <<<'GRAPHQL'
+directive @canAccess on FIELD_DEFINITION
+GRAPHQL;
+    }
+
+    /**
+     * Wrap around the final field resolver.
+     *
+     * @param  \Nuwave\Lighthouse\Schema\Values\FieldValue  $fieldValue
+     * @param  \Closure  $next
+     * @return \Nuwave\Lighthouse\Schema\Values\FieldValue
+     */
+    public function handleField(FieldValue $fieldValue, Closure $next)
+    {
+        $originalResolver = $fieldValue->getResolver();
+
+        return $next(
+            $fieldValue->setResolver(
+                function ($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) use ($originalResolver) {
+                    $role = $this->directiveArgValue('role');
+
+                    if ($role === null) {
+                        throw new DefinitionException("Missing argument 'role' for directive '@canAccess'.");
+                    }
+
+                    $user = $context->user();
+
+                    if (!$user || !$user->hasRole($role)) {
+                        return null;
+                    }
+
+                    return $originalResolver($root, $args, $context, $resolveInfo);
+                }
+            )
+        );
+    }
+}
