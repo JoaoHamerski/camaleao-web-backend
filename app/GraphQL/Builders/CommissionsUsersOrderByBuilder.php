5a007caeb0651a646dd902d19dfc4f2964f15d46
João Antônio <joaohamerski@hotmail.com>
2022-03-01 16:34:14 -0300

    🐛 Fix CommissionsUsers Graphql Builder for ordering

--- app/GraphQL/Builders/CommissionsUsersOrderByBuilder.php
+++ app/GraphQL/Builders/CommissionsUsersOrderByBuilder.php
@@ -8,11 +8,20 @@ class CommissionsUsersOrderByBuilder
 {
     public function __invoke(Builder $builder)
     {
-        return $builder->join(
-            'commissions',
-            'commission_user.commission_id',
-            '=',
-            'commissions.id'
-        )->orderBy('commissions.created_at', 'desc');
+        return $builder
+            ->join(
+                'commissions',
+                'commission_user.commission_id',
+                '=',
+                'commissions.id'
+            )
+            ->join(
+                'users',
+                'commission_user.user_id',
+                '=',
+                'users.id'
+            )
+            ->orderBy('commissions.created_at', 'desc')
+            ->select('commission_user.*');
     }
 }
