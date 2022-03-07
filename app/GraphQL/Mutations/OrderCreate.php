51423d03b03ed1fe42c6e84809ab496c783bfbac
João Hamerski <joaohamerski@hotmail.com>
2022-03-07 10:38:13 -0300

    🎨 Improve OrderCreate resolver: implement down payment on the resolver instead of calling the model method

--- app/GraphQL/Mutations/OrderCreate.php
+++ app/GraphQL/Mutations/OrderCreate.php
@@ -2,10 +2,10 @@
 
 namespace App\GraphQL\Mutations;
 
+use Carbon\Carbon;
+use App\Models\Order;
 use App\Models\Client;
-use Illuminate\Support\Arr;
 use App\GraphQL\Traits\OrderTrait;
-use Illuminate\Support\Facades\Auth;
 
 class OrderCreate
 {
@@ -18,6 +18,7 @@ class OrderCreate
     public function __invoke($_, array $args)
     {
         $data = $this->getFormattedData($args);
+
         $client = Client::find($args['client_id']);
 
         $this->validator($data)->validate();
@@ -34,17 +35,27 @@ class OrderCreate
             $this->handleCommissions($order);
         }
 
-        if (!empty($data['down_payment']) && !empty($data['payment_via_id'])) {
-            $payment = $order->createDownPayment(
-                $data['down_payment'],
-                $data['payment_via_id']
-            );
-
-            if (Auth::user()->hasRole('gerencia')) {
-                $payment->confirm();
-            }
+        if ($this->hasDownPayment($data)) {
+            $this->createDownPayment($order, $data);
         }
 
         return $order;
+    }
+
+    public function hasDownPayment($data)
+    {
+        return !empty($data['down_payment'])
+            && !empty($data['payment_via_id']);
+    }
+
+    public function createDownPayment($order, $data)
+    {
+        return (new PaymentCreate)->__invoke(null, [
+            'order_id' => $order->id,
+            'payment_via_id' => $data['payment_via_id'],
+            'value' => $data['down_payment'],
+            'date' => Carbon::now()->format('d/m/Y'),
+            'note'  => 'Pagamento de entrada'
+        ]);
     }
 }
