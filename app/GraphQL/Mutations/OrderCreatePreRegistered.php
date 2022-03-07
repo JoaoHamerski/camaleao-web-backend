c1a58f7e1ba7a946db4d7f63f842fe38fc630a6b
João Hamerski <joaohamerski@hotmail.com>
2022-03-07 10:52:33 -0300

    ✨ Add weekly production related: querie, resolver and schema

---
+++ app/GraphQL/Mutations/OrderCreatePreRegistered.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\Order;
+use App\Util\FileHelper;
+use App\Util\Formatter;
+use App\Util\Helper;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
+
+class OrderCreatePreRegistered
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $data = $this->getFormattedData($args);
+
+        $this->validator($data)->validate();
+
+        $data['art_paths'] = $this->storeFiles($data['art_paths']);
+
+        $order = Order::create(Arr::only($data, [
+            'status_id',
+            'art_paths',
+            'production_date'
+        ]));
+
+        if (Helper::filled($data, 'reminder')) {
+            $order->notes()->create([
+                'text' => $data['reminder'],
+                'is_reminder' => true
+            ]);
+        }
+
+        return $order;
+    }
+
+    public function getFormattedData($data)
+    {
+        return (new Formatter($data))
+            ->date('production_date')
+            ->base64ToUploadedFile('art_paths.*')
+            ->get();
+    }
+
+    public function storeFiles($files)
+    {
+        $paths = [];
+
+        foreach ($files as $key => $file) {
+            $paths[] = FileHelper::uploadFileToField($file, 'art_paths', $key);
+        }
+
+        return json_encode($paths);
+    }
+
+    public function validator($data)
+    {
+        return Validator::make($data, [
+            'art_paths' => ['array', 'min:1'],
+            'art_paths.*' => ['file', 'max:1024'],
+            'status_id' => ['nullable', 'exists:status,id'],
+            'reminder' => ['nullable']
+        ]);
+    }
+}
