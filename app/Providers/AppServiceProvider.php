b656d99f218d81ce1e79ae82670930075e344428
João Hamerski <joaohamerski@hotmail.com>
2022-03-09 18:52:36 -0300

    🎨 Remove custom blade directives and collection macros from PpServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,55 +31,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        Paginator::useBootstrap();
 
-        $this->bootBlade();
-        $this->bootCollectionMacros();
-    }
-
-    /**
-     * Inicializa as macros customizadas das Collections
-     *
-     * @return void
-     */
-    private function bootCollectionMacros()
-    {
-        Collection::macro('transpose', function () {
-            $items = [];
-            $arr = $this->all();
-
-            foreach ($arr as $key => $subarr) {
-                foreach ($subarr as $subkey => $subvalue) {
-                    $items[$subkey][$key] = $subvalue;
-                }
-            }
-
-            return new static($items);
-        });
-    }
-
-    /**
-     * Inicializa tudo relativo ao blade no service provider
-     *
-     * @return void
-     */
-    private function bootBlade()
-    {
-        $this->bootBladeValidation();
-        $this->bootBladeIfs();
-        $this->bootBladeComponents();
-    }
-
-    /**
-     * Estruturas de condições customizadas do blade
-     *
-     * @return void
-     */
-    private function bootBladeIfs()
-    {
-        Blade::if('role', function ($role) {
-            return (auth()->check() && auth()->user()->hasRole($role));
-        });
+        $this->bootValidationRules();
     }
 
     /**
@@ -87,29 +40,18 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    private function bootBladeValidation()
+    private function bootValidationRules()
     {
         Validator::extend('max_currency', 'App\Rules\MaxCurrency@passes');
         Validator::extend('min_currency', 'App\Rules\MinCurrency@passes');
         Validator::extend('equal', 'App\Rules\Equal@passes');
 
         Validator::replacer('max_currency', function ($message, $attribute, $rule, $parameters) {
-            return str_replace(':max', Mask::money($parameters[0]), $message);
+            return str_replace(':max', Mask::currencyBRL($parameters[0]), $message);
         });
 
         Validator::replacer('min_currency', function ($message, $attribute, $rule, $parameters) {
-            return str_replace(':min', Mask::money($parameters[0]), $message);
+            return str_replace(':min', Mask::currencyBRL($parameters[0]), $message);
         });
-    }
-
-    private function bootBladeComponents()
-    {
-        Blade::include('components.forms.input', 'input');
-        Blade::include('components.forms.data-list', 'dataList');
-        Blade::include('components.forms.input-file', 'inputFile');
-        Blade::include('components.forms.select', 'select');
-        Blade::include('components.modal', 'modal');
-        Blade::include('components.forms.radio', 'radio');
-        Blade::include('components.button', 'button');
     }
 }
