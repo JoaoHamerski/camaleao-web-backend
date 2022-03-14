d29fce0dbb82e2b279413e80fc4ae10df1f56438
João Hamerski <joaohamerski@hotmail.com>
2022-03-14 19:51:17 -0300

    🎨 Change LogsActivity trait that extends Spatie LogActivity

--- app/Traits/LogsActivity.php
+++ app/Traits/LogsActivity.php
@@ -2,203 +2,24 @@
 
 namespace App\Traits;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Collection;
-use Spatie\Activitylog\ActivityLogger;
-use Spatie\Activitylog\ActivitylogServiceProvider;
-use Spatie\Activitylog\ActivityLogStatus;
-use Spatie\Activitylog\Traits\DetectsChanges;
+use Spatie\Activitylog\Traits\LogsActivity as SpatieLogsActivity;
 
 trait LogsActivity
 {
-    use DetectsChanges {
-        attributeValuesToBeLogged as AVTBL;
-    }
+    use SpatieLogsActivity;
 
-    protected $enableLoggingModelsEvents = true;
-
-    public function attributeValuesToBeLogged(string $processingEvent): array
+    public function getDescriptionLog($type, $causer, $subject, $text)
     {
-        if (! count($this->attributesToBeLogged())) {
-            return [];
-        }
+        $causerProp = str_replace(':causer.', '', $causer);
+        $subjectProp = str_replace(':subject.', '', $subject);
 
-        $properties['attributes'] = static::logChanges(
-            $processingEvent == 'retrieved'
-                ? $this
-                : (
-                    $this->exists
-                        ? $this->fresh() ?? $this
-                        : $this
-                )
-        );
-
-        if (static::eventsToBeRecorded()->contains('updated') && $processingEvent == 'updated') {
-            $nullProperties = array_fill_keys(array_keys($properties['attributes']), null);
-
-            $properties['old'] = array_merge($nullProperties, $this->oldAttributes);
-
-            $this->oldAttributes = [];
-        }
-
-        if ($this->shouldLogOnlyDirty() && isset($properties['old'])) {
-
-            $properties['attributes'] = array_udiff_assoc(
-                $properties['attributes'],
-                $properties['old'],
-                function ($new, $old) {
-                    if ($old === null || $new === null) {
-                        return $new === $old ? 0 : 1;
-                    }
-
-                    if (is_object($new)) {
-                        return 1;
-                    }
-
-                    return $new <=> $old;
-                }
-            );
-
-            $properties['old'] = collect($properties['old'])
-                ->only(array_keys($properties['attributes']))
-                ->all();
-        }
-
-        return $properties;
-    }
-
-    protected static function bootLogsActivity()
-    {
-        static::eventsToBeRecorded()->each(function ($eventName) {
-            return static::$eventName(function (Model $model) use ($eventName) {
-                if (! $model->shouldLogEvent($eventName)) {
-                    return;
-                }
-
-                $description = $model->getDescriptionForEvent($eventName);
-
-                $logName = $model->getLogNameToUse($eventName);
-
-                if ($description == '') {
-                    return;
-                }
-
-                $attrs = $model->attributeValuesToBeLogged($eventName);
-
-                if ($model->isLogEmpty($attrs) && ! $model->shouldSubmitEmptyLogs()) {
-                    return;
-                }
-
-                $logger = app(ActivityLogger::class)
-                    ->useLog($logName)
-                    ->performedOn($model)
-                    ->withProperties($attrs);
-
-                if (method_exists($model, 'tapActivity')) {
-                    $logger->tap([$model, 'tapActivity'], $eventName);
-                }
-
-                $logger->log($description);
-            });
-        });
-    }
-
-    public function shouldSubmitEmptyLogs(): bool
-    {
-        return ! isset(static::$submitEmptyLogs) ? true : static::$submitEmptyLogs;
-    }
-
-    public function isLogEmpty($attrs): bool
-    {
-        return empty($attrs['attributes'] ?? []) && empty($attrs['old'] ?? []);
-    }
-
-    public function disableLogging()
-    {
-        $this->enableLoggingModelsEvents = false;
-
-        return $this;
-    }
-
-    public function enableLogging()
-    {
-        $this->enableLoggingModelsEvents = true;
-
-        return $this;
-    }
-
-    public function activities(): MorphMany
-    {
-        return $this->morphMany(ActivitylogServiceProvider::determineActivityModel(), 'subject');
-    }
-
-    public function getDescriptionForEvent(string $eventName): string
-    {
-        return $eventName;
-    }
-
-    public function getLogNameToUse(string $eventName = ''): string
-    {
-        if (isset(static::$logName)) {
-            return static::$logName;
-        }
-
-        return config('activitylog.default_log_name');
-    }
-
-    /*
-     * Get the event names that should be recorded.
-     */
-    protected static function eventsToBeRecorded(): Collection
-    {
-        if (isset(static::$recordEvents)) {
-            return collect(static::$recordEvents);
-        }
-
-        $events = collect([
-            'created',
-            'updated',
-            'deleted',
-        ]);
-
-        if (collect(class_uses_recursive(static::class))->contains(SoftDeletes::class)) {
-            $events->push('restored');
-        }
-
-        return $events;
-    }
-
-    public function attributesToBeIgnored(): array
-    {
-        if (! isset(static::$ignoreChangedAttributes)) {
-            return [];
-        }
-
-        return static::$ignoreChangedAttributes;
-    }
-
-    protected function shouldLogEvent(string $eventName): bool
-    {
-        $logStatus = app(ActivityLogStatus::class);
-
-        if (! $this->enableLoggingModelsEvents || $logStatus->disabled()) {
-            return false;
-        }
-
-        if (! in_array($eventName, ['created', 'updated'])) {
-            return true;
-        }
-
-        if (Arr::has($this->getDirty(), 'deleted_at')) {
-            if ($this->getDirty()['deleted_at'] === null) {
-                return false;
-            }
-        }
-
-        //do not log update event if only ignored attributes are changed
-        return (bool) count(Arr::except($this->getDirty(), $this->attributesToBeIgnored()));
+        return json_encode(compact(
+            'type',
+            'causer',
+            'causerProp',
+            'subject',
+            'subjectProp',
+            'text'
+        ));
     }
 }
