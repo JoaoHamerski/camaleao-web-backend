e8f6e794aa65c8ad93e0c2340ea10fbe71541dd4
João Hamerski <joaohamerski@hotmail.com>
2022-03-18 13:00:57 -0300

    🎨 Update ConfigSet mutation to updateOrInsert a key => value

--- app/GraphQL/Mutations/ConfigSet.php
+++ app/GraphQL/Mutations/ConfigSet.php
@@ -5,6 +5,7 @@ namespace App\GraphQL\Mutations;
 use App\Models\AppConfig;
 use Illuminate\Support\Facades\Validator;
 use App\GraphQL\Traits\ConfigTrait;
+use Illuminate\Validation\Rule;
 
 class ConfigSet
 {
@@ -22,11 +23,27 @@ class ConfigSet
 
         $config = AppConfig::where('name', $data['name'])->first();
 
-        $configDecoded = collect(json_decode($config->json));
-        $configDecoded = $configDecoded->merge([$data['key'] => $data['value']]);
+        $configDecoded = [$data['key'] => $data['value']];
 
-        $config->update(['json' => $configDecoded->toJson()]);
+        if ($config) {
+            $configDecoded = collect(json_decode($config->json));
+            $configDecoded = $configDecoded->merge([$data['key'] => $data['value']]);
+        }
+
+        $config = AppConfig::updateOrCreate(
+            ['name' => $data['name']],
+            ['json' => json_encode($configDecoded)]
+        );
 
         return $config;
+    }
+
+    public function validator($data)
+    {
+        return Validator::make($data, [
+            'name' => ['required'],
+            'key' => ['required'],
+            'value' => ['nullable']
+        ]);
     }
 }
