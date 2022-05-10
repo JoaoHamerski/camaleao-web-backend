51247d759a3f020a35e213f972bc6efb336e959e
João Hamerski <joaohamerski@hotmail.com>
2022-05-10 14:40:16 -0300

    Fix status update

---
+++ app/GraphQL/Mutations/OrderUpdateStatus.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\Order;
+use Illuminate\Support\Facades\Validator;
+
+class OrderUpdateStatus
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        Validator::make($args, [
+            'id' => ['required', 'exists:orders,id'],
+            'status_id' => ['required', 'exists:status,id']
+        ])->validate();
+
+        $order = Order::find($args['id']);
+        $order->update(['status_id' => $args['status_id']]);
+
+        return $order;
+    }
+}
