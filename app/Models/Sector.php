5e2c50147b5135314fac56f8a1ea955cf341d448
João Hamerski <joaohamerski@hotmail.com>
2022-09-09 12:00:04 -0300

    Fix

--- app/Models/Sector.php
+++ app/Models/Sector.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use App\Models\User;
-use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Sector extends Model
