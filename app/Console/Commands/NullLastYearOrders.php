04f63ace34387e6cd50ca684ebeab302b99bcf47
João Hamerski <joaohamerski@hotmail.com>
2023-02-02 16:32:43 -0300

    Update

---
+++ app/Console/Commands/NullLastYearOrders.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Order;
+use App\Models\OrderStatus;
+use App\Models\Status;
+use Illuminate\Console\Command;
+
+class NullLastYearOrders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        OrderStatus::join(
+            'orders',
+            'orders.id',
+            '=',
+            'order_status.order_id'
+        )->whereYear('orders.created_at', '2022')
+            ->whereBetween('order_status.created_at', [
+                '2023-01-23 09:20:00', '2023-01-23 09:30:00'
+            ])
+            ->select('order_status.*')
+            ->update(['created_at' => null]);
+
+        return 0;
+    }
+}
