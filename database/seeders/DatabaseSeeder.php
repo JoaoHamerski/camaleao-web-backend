ad748111dc784be928c88e62dadb032f01518a54
João Hamerski <joaohamerski@hotmail.com>
2023-06-01 03:10:02 -0300

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\SectorsSeeder;
-use Spatie\Activitylog\ActivityLogStatus;
 
 class DatabaseSeeder extends Seeder
 {
